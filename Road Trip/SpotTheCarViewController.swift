c52f092ce9ba7135c09689277ff512d36936735d
Aaron Whitfield <aawhitfield@hotmail.com>
2018-02-12 19:48:18 -0800

    Started table view on Spot the Car. Unsuccessful.

--- Road Trip/SpotTheCarViewController.swift
+++ Road Trip/SpotTheCarViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class SpotTheCarViewController: UIViewController {
+class SpotTheCarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
